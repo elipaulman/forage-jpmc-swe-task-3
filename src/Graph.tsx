20645349698ac4ecca57842c43db531c58bffa29
Elijah Paulman <eli.paulman@gmail.com>
2023-07-15 09:45:21 -0400

    first commit

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -23,10 +23,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price abc: 'float',
+      price def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper bound: 'float',
+      lower bound: 'float',
+      trigger alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -40,9 +43,12 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('row-pivots', '["timestamp"]');
       elem.setAttribute('columns', '["top_ask_price"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
         timestamp: 'distinct count',
       }));
     }
